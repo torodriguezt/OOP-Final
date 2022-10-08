224bdc0a55db41ca8414d04b19adc4fc52d6e847
caroAM22 <caalvarezmu@unal.edu.co>
2022-10-08 13:53:29 -0500

    Creación clase lista
    Se crearon los atributos, constructores y métodos de la clase lista. Como nombre, array de canciones y usuario. Con sus respectivos get y set. En métodos, agregarCancion, eliminarCancion y reescribí el toString.

--- src/gestorAplicacion/gestorMusica/Lista.java
+++ src/gestorAplicacion/gestorMusica/Lista.java
@@ -1,5 +1,65 @@
 package gestorAplicacion.gestorMusica;
 
-public class Lista {
+import java.util.ArrayList;
 
+import gestorAplicacion.gestorPersonas.*;
+
+public class Lista {
+	private ArrayList<Cancion> lista;
+	private Usuario usuario;
+	private String nombre;
+	
+	public Lista(String nombre, Usuario usuario) {
+		lista=new ArrayList<>();
+		this.usuario = usuario;
+		this.nombre=nombre;
+	}
+	
+	public Lista(String nombre,ArrayList<Cancion> lista, Usuario usuario) {
+		this.lista=lista;
+		this.usuario = usuario;
+		this.nombre=nombre;
+	}
+	
+	public ArrayList<Cancion> getListas() {
+		return lista;
+	}
+
+	public void setListas(ArrayList<Cancion> lista) {
+		this.lista = lista;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String agregarCancion(Cancion cancion) {
+		lista.add(cancion);
+		return "Se ha agregado la canción "+cancion.getNombre()+" a la lista "+nombre+" con exito";
+	}
+	
+	public String eliminarCancion(Cancion cancion) {
+		for (int i=0;i<lista.size();i++) {
+			if (cancion==lista.get(i)) {
+				lista.remove(i);
+			}
+		}
+		return "Se ha eliminado la canción "+cancion.getNombre()+" de la lista "+nombre+"con exito";
+	}
+	
+	public String ToString() {
+		return "Reproduciendo la lista "+nombre;
+	}
 }
