ab24704b24f41fbddb9fd3fc8bcbc8f10980fc5a
JEROLPOA2 <92282918+JEROLPOA2@users.noreply.github.com>
2022-10-12 11:19:32 -0500

    Update Musica.java

--- src/gestorAplicacion/gestorMusica/Musica.java
+++ src/gestorAplicacion/gestorMusica/Musica.java
@@ -3,6 +3,13 @@ public abstract class Musica {
     protected String nombre;
     protected int reproducciones;
 
+    public Musica(String nombre){
+
+        
+        this.nombre = nombre;
+        this.reproducciones = 0;
+    }
+
     public String getNombre() {
 		return nombre;
 	}
