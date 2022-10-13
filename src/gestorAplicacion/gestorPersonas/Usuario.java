44700139e618a24cc103fa07dc4a8438e125ab9c
Tomás Rodríguez <torodriguezt@unal.edu.co>
2022-10-13 09:41:37 -0500

    masEscuchada

--- src/gestorAplicacion/gestorPersonas/Usuario.java
+++ src/gestorAplicacion/gestorPersonas/Usuario.java
@@ -11,6 +11,7 @@ public class Usuario extends Persona {
     private Coleccion coleccion;
     private int tiempoEscuchado=0;
     private ArrayList <Artista> artistas;
+    private ArrayList <Cancion> canciones;
     
     public Usuario(String nombre){
     	super(nombre);
@@ -38,24 +39,50 @@ public class Usuario extends Persona {
     	artistas.add(cancion.getArtistas());
     }
     
-    //revisar donde comunicar ese artista con el usuario
+    public void agregarCancion(Cancion cancion) {
+    	canciones.add(cancion.getNombre());
+    }
+    
+    
+  //revisar donde comunicar esa cancion con las otras clases
+    public String topCancion(ArrayList <Cancion> canciones) {
+   	 int vecesReproducido = 0;
+   	 Cancion masEscuchada = null;
+   	 for (int i = 0; i < canciones.size(); i++) {
+   		 int contador = 0;
+   	     for (int j = 0; j < canciones.size(); j++) {
+   	        if (canciones.get(i) == canciones.get(j)) {
+   	          contador++;
+   	        }
+   	      }
+   	      if (contador > vecesReproducido) {
+   	        vecesReproducido = contador;
+   	        masEscuchada = canciones.get(i);
+   	      }
+   	 }
+   	    return "Tu cancion mas escuchado fue " + masEscuchada.getNombre();
+   }
+    
+    //revisar donde comunicar ese artista con las otras clases
     public String topArtista(ArrayList <Artista> artistas) {
     	 int vecesReproducido = 0;
     	 Artista masEscuchado = null;
     	 for (int i = 0; i < artistas.size(); i++) {
-    		 int count = 0;
+    		 int contador = 0;
     	     for (int j = 0; j < artistas.size(); j++) {
     	        if (artistas.get(i) == artistas.get(j)) {
-    	          count++;
+    	          contador++;
     	        }
     	      }
-    	      if (count > vecesReproducido) {
-    	        vecesReproducido = count;
+    	      if (contador > vecesReproducido) {
+    	        vecesReproducido = contador;
     	        masEscuchado = artistas.get(i);
     	      }
     	 }
     	    return "Tu artista mas escuchado fue " + masEscuchado.getNombre();
     }
+    
+    //plantear estadisticas como funcionalidad compleja entre clases junto con tiempo total
     
     public void setTiempoEscuchado(int tiempoEscuchado) {
         this.tiempoEscuchado = tiempoEscuchado;
