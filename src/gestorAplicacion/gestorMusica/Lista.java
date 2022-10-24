5bd1a64229e2c4bacf35a998bef49d26bcfbe80b
JEROLPOA2 <jledesmap@unal.edu.co>
2022-10-24 17:52:10 -0500

    Revert "Lista y Usuario"
    This reverts commit d83238a2499860c9c4f85b3b65b44f8ecf470f55.

--- src/gestorAplicacion/gestorMusica/Lista.java
+++ src/gestorAplicacion/gestorMusica/Lista.java
@@ -1,48 +1,25 @@
 package gestorAplicacion.gestorMusica;
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.Set;
-
 import gestorAplicacion.gestorPersonas.*;
 
-public class Lista extends Musica implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	private static ArrayList<Lista> listasExistentes;
+public class Lista extends Musica {
 	private ArrayList<Cancion> lista;
-	private Set<Cancion> listaColaborativa;
-	protected ArrayList<Usuario> usuarios;
-	protected Usuario usuario;
+	private Usuario usuario;
+	private int duracionTotal;
 	private String descripcion;
 	
-	static {
-		setListasExistentes(new ArrayList<Lista>());
-	}
-	
-	public Lista(String nombre, Usuario usuario, String descripcion) {
+	public Lista(String nombre, Usuario usuario) {
 		super(nombre);
-		lista = new ArrayList<>();
+		lista=new ArrayList<>();
 		this.usuario = usuario;
-		this.descripcion = descripcion;
-		this.usuario.getColeccion().agregarLista(this);
-		listasExistentes.add(this);
 	}
 	
-	public Lista(String nombre, ArrayList<Cancion> lista, Usuario usuario, String descripcion) {
-		this(nombre, usuario, descripcion);
-		this.lista = lista;
-	}
-	
-	public Lista(String nombre, ArrayList<Usuario> usuarios, Set<Cancion> canciones) {
+	public Lista(String nombre,ArrayList<Cancion> lista, Usuario usuario) {
 		super(nombre);
-		this.usuarios = usuarios;
-		this.listaColaborativa = canciones;
-		//ver como afecta que en el constructor solo reciba usuarios invidivuales en el caso de tener arrays 
-		this.colaborativa(usuarios);
+		this.lista=lista;
+		this.usuario = usuario;
 	}
-	
 	
 	public ArrayList<Cancion> getLista() {
 		return lista;
@@ -50,14 +27,6 @@ public class Lista extends Musica implements Serializable {
 
 	public void setLista(ArrayList<Cancion> lista) {
 		this.lista = lista;
-	}
-
-	public Set<Cancion> getListaColaborativa() {
-		return listaColaborativa;
-	}
-
-	public void setListaColaborativa(Set<Cancion> listaColaborativa) {
-		this.listaColaborativa = listaColaborativa;
 	}
 
 	public Usuario getUsuario() {
@@ -68,26 +37,9 @@ public class Lista extends Musica implements Serializable {
 		this.usuario = usuario;
 	}
 	
-	public static ArrayList<Lista> getListasExistentes() {
-		return listasExistentes;
-	}
-
-	public static void setListasExistentes(ArrayList<Lista> listasExistentes) {
-		Lista.listasExistentes = listasExistentes;
-	}
-
-	public ArrayList<Usuario> getUsuarios() {
-		return usuarios;
-	}
-
-	public void setUsuarios(ArrayList<Usuario> usuarios) {
-		this.usuarios = usuarios;
-	}
-	
-	public int duracionLista() {
-		int duracionTotal=0;
-		for (int i = 0; i<lista.size();i++) {
-			duracionTotal = lista.get(i).getDuracion()+duracionTotal;
+	public int duracionLista(Lista elementos) {
+		for (int i = 0; i<elementos.lista.size();i++) {
+			duracionTotal = elementos.lista.get(i).getDuracion()+duracionTotal;
 		}
 		return duracionTotal;
 		
@@ -103,48 +55,15 @@ public class Lista extends Musica implements Serializable {
 		return "Se ha eliminado la canción "+cancion.getNombre()+" de la lista "+nombre+"con exito";
 	}
 	
-	@Override
 	public String toString() {
-		return "Se está reproduciendo la lista " + nombre;
-	}
-	
-	public String infoLista() {
-		String des = "";
-		if (lista.size() > 0) {
-			for (Cancion cancion : lista) {
-				des += cancion.descripcion() + "\n";
-			}
-			return "Lista: " + nombre + "\n" + "\n" + "Canciones: " + "\n" + des;
-		} else {
-			return "La lista: " + nombre + " está vacía\n";
-		}
-	}
-	
-	public String infoColaborativa() {
-		String des = "";
-		if (listaColaborativa.size() > 0) {
-			for (Cancion cancion : listaColaborativa) {
-				des += cancion.descripcion() + "\n";
-			}
-			return "Lista: " + nombre + "\n" + "\n" + "Canciones: " + "\n" + des;
-		} else {
-			return "La lista: " + nombre + " está vacía\n";
-		}
+		return "Reproduciendo la lista "+nombre;
 	}
 
-	@Override
 	public void aumentarReproducciones(){
 		this.reproducciones++; 
-		for (int i = 0; i < lista.size(); i++) {
+		for (int i=0;i<lista.size();i++) {
 			lista.get(i).aumentarReproducciones();
 		}
-	}
-	
-	public String colaborativa(ArrayList<Usuario> usuarios) { //Para que se asigne individualmente a cada usuario
-		for(Usuario persona: usuarios) {
-			persona.getColeccion().agregarLista(this, persona);
-		}
-		return "Lista Colaborativa creada";
 	}
 	
 	public String getDescripcion() {
@@ -154,25 +73,17 @@ public class Lista extends Musica implements Serializable {
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
-	public static ArrayList<Cancion> listaPorGenero(Genero genero){
-		ArrayList<Cancion> cancionGenero = new ArrayList<Cancion>();
-		
-		for(Cancion existentes: Cancion.getCancionesDisponibles()) {
-			if(existentes.getGenero().equals(genero)) {
-				cancionGenero.add(existentes);
-			}
-		} return cancionGenero;
+
+	public ArrayList<Cancion> ordenarPorNombre(Lista lista){
+		lista.sort(Comparator.comparing(Cancion::getName()).thenComparing(Cancion::getArtista().getNombre()));
+		return lista;
 	}
-		
-	public int totalPorGenero(Genero genero) {
-		
-		int total = 0;
-		for(Cancion cancion: getLista()) {
-			if(cancion.getGenero().equals(genero)) {
-				total += 1;
-			}
-		}
-		return total;
+	public ArrayList<Cancion> ordenarPorAno(Lista lista){
+		lista.sort(Comparator.comparing(Cancion::getAno()));
+		return lista;
+	}
+	public ArrayList<Cancion> ordenarPorReproduciones(Lista lista){
+		lista.sort(Comparator.comparing(Cancion::getReproduciones()));
+		return lista;
 	}
 }
